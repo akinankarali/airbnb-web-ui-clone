67072a96f2a56316de169cc68f07fef9e884248e
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-12-30 19:07:49 +0300

    add constants

---
+++ airbnbui/lib/constants.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+const kTextColor = Color(0xFF222222);
+const kTextColor2 = Color(0xFF48484848);
+
+const kSearchButton = Color(0xFFFFF385C);
+const kSearchbarHover = Color(0xFFEBEBEB);
+
+const kBackgroundColor = Color(0xFFF7F7F7);
+
+const kDefaultPadding = 20.0;
+
+const kTextFontSize1 = 12.0;
+const kTextFontSize2 = 14.0;
+const kTextFontSize3 = 16.0;
+const kTextFontSize4 = 18.0;
+const kTextFontSize5 = 32.0;
+const kTextFontSize6 = 48.0;
+const kTextFontSize7 = 108.0;
